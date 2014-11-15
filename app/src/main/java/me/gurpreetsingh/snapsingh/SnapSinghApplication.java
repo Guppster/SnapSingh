4498c2b21ed9c893aab86209a580c583966c7933
Guppster <guppst3r@gmail.com>
2014-11-14 20:30:02 -0500

    Started using parse

---
+++ app/src/main/java/me/gurpreetsingh/snapsingh/SnapSinghApplication.java
@@ -0,0 +1,23 @@
+package me.gurpreetsingh.snapsingh;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+/**
+ * Created by Gurpreet on 2014-11-14.
+ */
+public class SnapSinghApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        Parse.initialize(this, "WRFWXyfaLViNErFpnQxA5BoKXiG6vtvB0leyv7pX", "P3vUl9yu71rRCybSka1VQKB24036DrVwiWA0OKXJ");
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
