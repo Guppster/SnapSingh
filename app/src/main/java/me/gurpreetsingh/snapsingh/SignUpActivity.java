a9ccc2993f47099fd5c787f0977be52d419a314a
Guppster <guppst3r@gmail.com>
2014-11-14 20:00:11 -0500

    Added sign up page

--- app/src/main/java/me/gurpreetsingh/snapsingh/SignUpActivity.java
+++ app/src/main/java/me/gurpreetsingh/snapsingh/SignUpActivity.java
@@ -1,41 +1,26 @@
 package me.gurpreetsingh.snapsingh;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 
 
-public class LoginActivity extends Activity
-{
-    protected TextView mSignupTextView;
+public class SignUpActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        mSignupTextView = (TextView)findViewById(R. id.signUpButton);
-        mSignupTextView.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }});
+        setContentView(R.layout.activity_sign_up);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
         return true;
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
